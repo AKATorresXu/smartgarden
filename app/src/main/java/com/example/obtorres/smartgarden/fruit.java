a166b43f11fa5a85291ddd6580f84757ad9c3960
TorresXu <torres@163.com>
2018-08-23 11:32:01 +0800

    first commit.

---
+++ app/src/main/java/com/example/obtorres/smartgarden/fruit.java
@@ -0,0 +1,29 @@
+package com.example.obtorres.smartgarden;
+
+public class fruit {
+    private String name;
+
+
+
+    private String family;
+    private int imageID;
+    public String getName() {
+        return name;
+    }
+    public String getFamily() {
+        return family;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+
+
+    public fruit(String name,String family, int imageID) {
+        this.name = name;
+        this.imageID = imageID;
+        this.family=family;
+    }
+
+}
